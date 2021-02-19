b6701986272b8b7c8faa8522eeceba2b22fd0730
Francisco Aurizelio <francisco.souza63@etec.sp.gov.br>
2021-02-18 22:47:55 -0300

    operadores aritmetico e comparacao

---
+++ OperadorComparacao.php
@@ -0,0 +1,12 @@
+<?php
+
+$nota = 7;
+$notaTrabalho = 8;
+
+$resultado = $nota >=  $notaTrabalho;
+
+$xpto = boolval($resultado) ? 'true' : 'false';
+
+echo "Resultado ".$xpto;
+
+?>
---
+++ operador.php
@@ -0,0 +1,12 @@
+<?php
+
+//Operadores Aritméticos 
+$numero1 = 10;
+$numero = 3;
+
+$resultado = $numero1 ** $numero;
+
+//Texto concatenado com uma variavel
+echo "Resultado ".$resultado;
+
+?>
